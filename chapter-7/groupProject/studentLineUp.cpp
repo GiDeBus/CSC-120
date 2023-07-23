f02a2b6099ee66f167dc9a583866d509be25676a
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-07-23 18:27:02 -0400

    Add group project source code

--- chapter-7/groupProject/studentLineUp.cpp
+++ chapter-7/groupProject/studentLineUp.cpp
@@ -1,23 +1,62 @@
-# Group Project: Student Line Up
+/* 
+  Programmer: Gil Del Busto
+  Program: Student Organizer Group Project
+  Description: This program prompts the user for a number of students. 
+  It writes a file and outputs the first and last students. 
+*/
 
-A teacher asked all her students to line up according to their first names. 
-For example, in one class Amy will be at the front of the line and, Yolanda would be at the end.
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
 
-Write a program that: 
+using namespace std;
 
-[] - prompts the user to enter the number of students in the class, then 
-[] - loops to read that many names and 
-[] - save the names to a file called LineUp.txt.
+int main() {
+    int numStudents;
+    string name;
+    string inputFileName = "LineUp.txt";
+    string frontStudent, endStudent;
 
-Next, the program should: 
-[] - read those names from the LineUp.txt file. 
-      Names should be read until there is no more data to read. 
-      Once all names have been read, 
-[] - it reports which student would be in front of the line and 
-[] - which one would be at the end of the line.
+    // Prompt the user to enter the number of students in the class
+    while (true) {
+        cout << "Enter the number of students in the class (1 to 25): ";
+        if (cin >> numStudents && numStudents >= 1 && numStudents <= 25) {
+            break;
+        }
+        cout << "Invalid input. Please enter a number between 1 and 25." << endl;
+        cin.clear();
+        cin.ignore(100, '\n');
+    }
 
-- You may assume that no two students have the same name.
+    // Loop to read names and save them to the LineUp.txt file
+    ofstream outFile(inputFileName);
+    if (!outFile.fail()) {
+        cin.ignore(); // Ignore the newline character from the previous input
+        for (int i = 0; i < numStudents; i++) {
+            cout << "Enter the name of student " << i + 1 << ": ";
+            getline(cin, name);
+            outFile << name << endl;
 
-[] - Input validation: Do not accept a number less than  1 or greater than 25 for the number of students.
+            // Determine the student in front of the line and at the end of the line
+            if (i == 0) {
+                frontStudent = name;
+                endStudent = name;
+            } else {
+                frontStudent = min(frontStudent, name);
+                endStudent = max(endStudent, name);
+            }
+        }
+        outFile.close();
+        cout << "Names have been saved to " << inputFileName << "." << endl;
+    } else {
+        cout << "Error: Unable to create " << inputFileName << "." << endl;
+        return 1;
+    }
 
-You can use the attached file to test your program.
+    // Report the student in front of the line and at the end of the line
+    cout << "The student in front of the line: " << frontStudent << endl;
+    cout << "The student at the end of the line: " << endStudent << endl;
+
+    return 0;
+}
