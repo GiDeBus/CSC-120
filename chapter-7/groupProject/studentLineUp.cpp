3d5f2876b1f179baa1546c19424ad8a06341c75f
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-07-23 19:20:37 -0400

    Refactor naming of variables

--- chapter-7/groupProject/studentLineUp.cpp
+++ chapter-7/groupProject/studentLineUp.cpp
@@ -13,50 +13,50 @@
 using namespace std;
 
 int main() {
-    int numStudents;
-    string name;
-    string inputFileName = "LineUp.txt";
-    string frontStudent, endStudent;
+  int numberOfStudents;
+  string name;
+  string inputFileName = "LineUp.txt";
+  string frontOfLineStudent, endOfLineStudent;
 
-    // Prompt the user to enter the number of students in the class
-    while (true) {
-        cout << "Enter the number of students in the class (1 to 25): ";
-        if (cin >> numStudents && numStudents >= 1 && numStudents <= 25) {
-            break;
-        }
-        cout << "Invalid input. Please enter a number between 1 and 25." << endl;
-        cin.clear();
-        cin.ignore(100, '\n');
+  // Prompt the user to enter the number of students in the class
+  while (true) {
+    cout << "Enter the number of students in the class (1 to 25): ";
+    if (cin >> numberOfStudents && numberOfStudents >= 1 && numberOfStudents <= 25) {
+      break;
     }
+    cout << "Invalid input. Please enter a number between 1 and 25." << endl;
+    cin.clear();
+    cin.ignore(100, '\n');
+  }
 
-    // Loop to read names and save them to the LineUp.txt file
-    ofstream outFile(inputFileName);
-    if (!outFile.fail()) {
-        cin.ignore(); // Ignore the newline character from the previous input
-        for (int i = 0; i < numStudents; i++) {
-            cout << "Enter the name of student " << i + 1 << ": ";
-            getline(cin, name);
-            outFile << name << endl;
+  // Loop to read names and save them to the LineUp.txt file
+  ofstream outFile(inputFileName);
+  if (!outFile.fail()) {
+    cin.ignore(); // Ignore the newline character from the previous input
+    for (int i = 0; i < numberOfStudents; i++) {
+      cout << "Enter the name of student " << i + 1 << ": ";
+      getline(cin, name);
+      outFile << name << endl;
 
-            // Determine the student in front of the line and at the end of the line
-            if (i == 0) {
-                frontStudent = name;
-                endStudent = name;
-            } else {
-                frontStudent = min(frontStudent, name);
-                endStudent = max(endStudent, name);
-            }
-        }
-        outFile.close();
-        cout << "Names have been saved to " << inputFileName << "." << endl;
-    } else {
-        cout << "Error: Unable to create " << inputFileName << "." << endl;
-        return 1;
+      // Determine the student in front of the line and at the end of the line
+      if (i == 0) {
+        frontOfLineStudent = name;
+        endOfLineStudent = name;
+      } else {
+        frontOfLineStudent = (frontOfLineStudent < name) ? frontOfLineStudent : name;
+        endOfLineStudent = (endOfLineStudent > name) ? endOfLineStudent : name;
+      }
     }
+    outFile.close();
+    cout << "Names have been saved to " << inputFileName << "." << endl;
+  } else {
+    cout << "Error: Unable to create " << inputFileName << "." << endl;
+    return 1;
+  }
 
-    // Report the student in front of the line and at the end of the line
-    cout << "The student in front of the line: " << frontStudent << endl;
-    cout << "The student at the end of the line: " << endStudent << endl;
+  // Report the student in front of the line and at the end of the line
+  cout << "The student in front of the line: " << frontOfLineStudent << endl;
+  cout << "The student at the end of the line: " << endOfLineStudent << endl;
 
-    return 0;
+  return 0;
 }
