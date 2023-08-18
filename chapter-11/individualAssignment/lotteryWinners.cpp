a62ce6f9578815fc9e63abfba05056a205585ef6
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-08-18 15:15:45 -0400

    Add folder for individual assignment and basic source code

---
+++ chapter-11/individualAssignment/lotteryWinners.cpp
@@ -0,0 +1,17 @@
+/* 
+  Programmer: Gil Del Busto
+  Program: Lottery Winners
+  Description: This program performs a linear search to see if 
+    an individual won the lottery based on winning numbers. 
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+}
